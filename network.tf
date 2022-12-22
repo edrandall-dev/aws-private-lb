b856d90ebc4610255ce4d0e110c2046084b97681
edrandall-dev <ed@edrandall.uk>
2022-12-22 13:58:46 +0000

    Routes & Route Tables Done for IGW & NGW

--- network.tf
+++ network.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "ed_privlb_vpc" {
 resource "aws_subnet" "ed_privlb_public_subnet" {
   count = 2 //Three subnets are required (across 3 AZs)
 
-  vpc_id = aws_vpc.ed_privlb_vpc.id
+  vpc_id                  = aws_vpc.ed_privlb_vpc.id
   cidr_block              = cidrsubnet(var.base_cidr_block, 8, count.index + 1)
   availability_zone       = join("", ["${var.region}", "${var.availability_zones[count.index]}"])
   map_public_ip_on_launch = false
@@ -28,7 +28,7 @@ resource "aws_subnet" "ed_privlb_public_subnet" {
 resource "aws_subnet" "ed_privlb_private_subnet" {
   count = 2 //Three subnets are required (across 3 AZs)
 
-  vpc_id = aws_vpc.ed_privlb_vpc.id
+  vpc_id                  = aws_vpc.ed_privlb_vpc.id
   cidr_block              = cidrsubnet(var.base_cidr_block, 8, count.index + 10)
   availability_zone       = join("", ["${var.region}", "${var.availability_zones[count.index]}"])
   map_public_ip_on_launch = false
@@ -49,6 +49,28 @@ resource "aws_internet_gateway" "ed-igw" {
     "Terraform" = true
   }
 }
+
+resource "aws_eip" "ed-ngw-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "ed-ngw" {
+  allocation_id = aws_eip.ed-ngw-eip.id
+
+  //Put the NGW in the first public subnet
+  subnet_id = aws_subnet.ed_privlb_public_subnet[0].id
+
+  tags = {
+    "Name"      = "${var.env_prefix}_vpc_ngw"
+    "Creator"   = var.creator
+    "Terraform" = true
+  }
+
+  //To ensure proper ordering, it is recommended to add an explicit dependency
+  //on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.ed-igw]
+}
+
 
 resource "aws_route_table" "ed_public_subnet_rt" {
   vpc_id = aws_vpc.ed_privlb_vpc.id
@@ -74,9 +96,20 @@ resource "aws_route" "ed_public_subnet_route" {
   gateway_id             = aws_internet_gateway.ed-igw.id
 }
 
-
 resource "aws_route_table_association" "edr_public_route_assoc" {
-  count = 2
-  subnet_id                 = aws_subnet.ed_privlb_public_subnet[count.index].id
+  count          = 2
+  subnet_id      = aws_subnet.ed_privlb_public_subnet[count.index].id
   route_table_id = aws_route_table.ed_public_subnet_rt.id
+}
+
+resource "aws_route" "ed_private_subnet_route" {
+  route_table_id         = aws_route_table.ed_private_subnet_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.ed-ngw.id
+}
+
+resource "aws_route_table_association" "edr_private_route_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.ed_privlb_private_subnet[count.index].id
+  route_table_id = aws_route_table.ed_private_subnet_rt.id
 }
