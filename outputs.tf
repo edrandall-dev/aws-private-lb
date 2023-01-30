00a3802c90259ee9384426c986f6ae51e9259964
Ed Randall <ed.randall@neo4j.com>
2023-01-30 10:22:33 +0000

    Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "loadbalacer_address" {
+output "loadbalancer_address" {
   description = "The URL of the Application Load Balancer"
   value       = "http://${aws_lb.test_env_alb.dns_name}"
 }
